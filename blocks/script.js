5e36bad08598d449a52e4b63c7769b447bf9794d
Sofa Ivahnenko <sofyaivahnencko@yandex.ru>
2022-10-06 18:21:48 +0300

    1

--- blocks/script.js
+++ blocks/script.js
@@ -1,28 +1,42 @@
-const popupOpenButton = document.querySelector('.profile__add-button');
 const popup = document.querySelector('.popup');
-const popupCloseButton = popup.querySelector('.popup__close');
-const popupSaveButton = popup.querySelector('.popup__button');
+const popupOpenButton = document.querySelector('.profile__button-edit');
+const popupCloseButton = popup.querySelector('.popup__close-button');
+const popupSaveButton = popup.querySelector('.popup__save-button');
 const popupToggle = function () {
     popup.classList.toggle('popup__opened');
 }
 
-let formElement = document.querySelector('.popup__content');
+let formElement = document.querySelector('.popup__form');
 let nameInput = popup.querySelector('.popup__form_name');
 let jobInput = popup.querySelector('.popup__form_composition');
 let person = document.querySelector('.profile__title');
 let about = document.querySelector('.profile__subtitle');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
+function popupOpen() {
+    popup.classList.remove('popup_opened');
+}
+
+function popupClose() {
+    popup.classList. remove('popup_opened');
+}
+
+function popupCloseByClickingOverlay (event) {
+    if (event.target == event.currentTarget) {
+        popupClose();
+    }
+}
+
+popupOpenButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
+popup.addEventListener('click', popupCloseByClickingOverlay);
+
+function popupSave (evt) {
     evt.preventDefault(); 
     person.textContent = nameInput.value;
     about.textContent = jobInput.value;
     nameInput.placeholder = 'Имя';
     jobInput.placeholder = 'О себе';
+    popupClose();
 }
 
-popupOpenButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
-popupSaveButton.addEventListener('click', popupToggle);
 formElement.addEventListener('submit', formSubmitHandler); 
