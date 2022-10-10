f98cccdf67270e7b7574663286bb62ff577c8dcc
Sofa Ivahnenko <sofyaivahnencko@yandex.ru>
2022-10-10 10:00:06 +0300

    first_at

--- script/index.js
+++ script/index.js
@@ -2,9 +2,6 @@ const popup = document.querySelector('.popup');
 const popupOpenButton = document.querySelector('.profile__button-edit');
 const popupCloseButton = popup.querySelector('.popup__close-button');
 const popupSaveButton = popup.querySelector('.popup__save-button');
-const popupToggle = function () {
-    popup.classList.toggle('popup__opened');
-}
 
 let formElement = document.querySelector('.popup__form');
 let nameInput = popup.querySelector('.popup__form_name');
@@ -13,7 +10,9 @@ let person = document.querySelector('.profile__title');
 let about = document.querySelector('.profile__subtitle');
 
 function popupOpen() {
-    popup.classList.remove('popup_opened');
+    popup.classList.add('popup_opened');
+    nameInput.value = person.textContent;
+    jobInput.value = about.textContent;
 }
 
 function popupClose() {
@@ -28,15 +27,13 @@ function popupCloseByClickingOverlay (event) {
 
 popupOpenButton.addEventListener('click', popupOpen);
 popupCloseButton.addEventListener('click', popupClose);
-popup.addEventListener('click', popupCloseByClickingOverlay);
+popupSaveButton.addEventListener('click', popupSave);
 
 function popupSave (evt) {
     evt.preventDefault(); 
     person.textContent = nameInput.value;
     about.textContent = jobInput.value;
-    nameInput.placeholder = 'Имя';
-    jobInput.placeholder = 'О себе';
     popupClose();
 }
 
-formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', popupSave); 
