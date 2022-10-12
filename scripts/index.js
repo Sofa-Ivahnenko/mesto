f187c5ff81d802cc4a83b96427f527aaf4b16ea4
Sofa Ivahnenko <sofyaivahnencko@yandex.ru>
2022-10-12 10:26:43 +0300

    changed

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const popupCloseButton = popup.querySelector('.popup__close-button');
 const popupSaveButton = popup.querySelector('.popup__save-button');
 
 let formElement = document.querySelector('.popup__form');
-let nameInput = popup.querySelector('.popup__form_name');
-let jobInput = popup.querySelector('.popup__form_composition');
+let nameInput = popup.querySelector('.popup__input_type_name');
+let jobInput = popup.querySelector('.popup__input_type_composition');
 let person = document.querySelector('.profile__title');
 let about = document.querySelector('.profile__subtitle');
 
@@ -16,24 +16,18 @@ function popupOpen() {
 }
 
 function popupClose() {
-    popup.classList. remove('popup_opened');
+    popup.classList.remove('popup_opened');
 }
 
-function popupCloseByClickingOverlay (event) {
-    if (event.target == event.currentTarget) {
-        popupClose();
-    }
-}
+function popupSave(evt) {
+    evt.preventDefault(); 
+    person.textContent = nameInput.value;
+    about.textContent = jobInput.value;
+    popupClose();
+  }
 
 popupOpenButton.addEventListener('click', popupOpen);
 popupCloseButton.addEventListener('click', popupClose);
 popupSaveButton.addEventListener('click', popupSave);
 
-function popupSave (evt) {
-    evt.preventDefault(); 
-    person.textContent = nameInput.value;
-    about.textContent = jobInput.value;
-    popupClose();
-}
-
-formElement.addEventListener('submit', popupSave); 
+formElement.addEventListener('submit', popupSave);
