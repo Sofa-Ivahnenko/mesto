d070ac864e4af91e623d0dc78b5b1d42b95b40c7
Sofa Ivahnenko <sofyaivahnencko@yandex.ru>
2023-02-11 21:56:13 +0300

    oop

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import PopupWithForm from '../components/PopupWithForm.js';
 import Section from '../components/Section.js';
 import UserInfo from '../components/UserInfo.js';
 import PopupWithImage from '../components/PopupWithImage.js';
+import './index.css';
 
 
 
